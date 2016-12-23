8018f5c054c0d211795c152119445ab16b4b2a38
saguila <saguila@ucm.es>
2016-12-23 02:35:35 +0100

    Primer commit, ya implementadas las sesiones

---
+++ bin/sesiones.js
@@ -0,0 +1,21 @@
+var config =  require('../config');
+var express = require('express');
+var session = require('express-session');
+var MYSQLStore = require('express-mysql-session')(session);
+module.exports = session({
+    saveUninitialized: false, // No guardar la sesion mientras que no se añada algo al objecto session.
+    resave: false, // No guardar la sesion a  menos que se modifique.
+    secret:'prueba',
+    unset: 'destroy', // Si session=null se destruye de la BD.
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    },
+    store: new MYSQLStore({
+        autoRemove: 'interval',
+        host:config.DB.host,
+        user:config.DB.user,
+        password:config.DB.password,
+        database:config.DB.database,
+        autoRemoveInterval: 10 // In minutes. Default
+    })
+})
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	DB : {
+		host: 'localhost',
+		user: 'academia',
+		password: '3,14016pi',
+		database: 'academia',
+		port: 3306
+	}
+	,
+	APPPORT:3000
+
+}
